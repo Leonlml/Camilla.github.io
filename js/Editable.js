5b19b66075f97a3dfa6a43501bd166b7e2d6ede7
Leonlml <124469004+Leonlml@users.noreply.github.com>
2023-09-06 14:09:33 -0600

    inicial commit

---
+++ js/Editable.js
@@ -0,0 +1,5 @@
+
+function editar(){
+document.getElementById('questions container').contentEditable = 'true';
+document.getElementById('questions container').designMode='on';}
+window.onload=function(){editar();}
